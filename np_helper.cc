15923ae09041a9cc3c500c68e3fea55b01a78bc8
boton <botonchou@gmail.com>
2015-06-11 23:44:36 +0800

    Remove *.o and binary from git

--- np_helper.cc
+++ np_helper.cc
@@ -29,6 +29,22 @@ vector<string> split(const string &s, char delim) {
 }
 */
 
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+  std::stringstream ss(s);
+  std::string item;
+  while (std::getline(ss, item, delim)) {
+    elems.push_back(item);
+  }
+  return elems;
+}
+
+
+std::vector<std::string> split(const std::string &s, char delim) {
+  std::vector<std::string> elems;
+  split(s, delim, elems);
+  return elems;
+}
+
 SAMPLE read_struct_examples_helper(char *labelfile, char *wordFeaturefile, char *predicateFeaturefile, char *structfile, STRUCT_LEARN_PARM *sparm) {
 
   // file - word - fea
